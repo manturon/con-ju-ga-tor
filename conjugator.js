3eb028d3689a63a1e8d46f601f2f7afc636c7ace
manturon <manturon@mm.st>
2025-06-05 18:46:08 -0400

    Add other tenses, display 10 random verbs, fix cheat key

--- conjugator.js
+++ conjugator.js
@@ -1,6 +1,15 @@
 import { render } from 'preact';
-import { useState, useEffect } from 'preact/hooks';
+import { useState, useEffect, useRef, useImperativeHandle } from 'preact/hooks';
 import { html } from 'htm/preact';
+
+twind.install({
+  presets: [twind.presetExt(), twind.presetTypography()],
+  theme: {
+    extend: {
+      fontFamily: { sans: 'Sono, sans-serif' },
+    },
+  },
+});
 
 const VERBS = await fetch('./verbs.json', { cache: 'force-cache' }).then(
   (res) => res.json()
@@ -34,6 +43,15 @@ const REGULAR_ENDINGS = {
   },
 };
 
+const TENSES = [
+  'presente',
+  'imperfecto',
+  'pretérito',
+  'futuro',
+  'condicional',
+  'subjuntivo',
+];
+
 function regularConjugate(verb) {
   const root = verb.slice(0, -2);
   const ending = verb.slice(-2);
@@ -66,81 +84,140 @@ function conjugate(verb) {
 
 const PRONOUNS = ['yo', 'tú', 'él', 'nosotros', 'ellos'];
 
-function InputValue({ conjugation, pronoun, fill }) {
-  const [input, setInput] = useState('');
-  const correct = input.trim().toLowerCase() === conjugation;
-  const empty = !!input.trim();
-  return html`<div class="flex flex-col gap-2 items-center">
+function InputValue({ conjugation, pronoun, onInput, value }) {
+  let wide = conjugation.length > 10;
+
+  let correct = value.trim().toLowerCase() === conjugation;
+  let empty = !!value.trim();
+
+  return html`<div
+    class="flex flex-col gap-2 items-center ${wide ? 'w-48' : 'w-32'}"
+  >
     <input
-      class="border-b-2 text-xl outline-none w-32 ${empty
+      class="border-b-2 text-xl outline-none ${wide ? 'w-48' : 'w-32'} ${empty
         ? correct
           ? 'bg-green-100'
           : 'bg-red-100'
         : ''}"
       type="text"
-      value=${fill ? conjugation : input}
-      disabled=${fill}
-      onInput=${({ target }) => {
-        if (!fill) {
-          setInput(target.value);
-        }
-      }}
+      value=${value}
+      onInput=${onInput}
     />
     <span class="text-xl text-gray-700">${pronoun}</span>
   </div>`;
 }
 
-function PageButton({ side = 'right', disabled = false, ...rest }) {
+function Exercise({
+  verb,
+  tense,
+  conjugations,
+  initialValues,
+  onInput,
+  handleRef,
+}) {
+  const [values, setValues] = useState(
+    initialValues ?? new Array(conjugations.length).fill('')
+  );
+
+  useImperativeHandle(
+    handleRef,
+    () => ({
+      fill() {
+        let v = [...conjugations];
+        setValues(v);
+        onInput.call(null, v);
+      },
+    }),
+    []
+  );
+
+  const input =
+    (i) =>
+    ({ target }) => {
+      let v = [...values];
+      v[i] = target.value;
+      setValues(v);
+      onInput.call(null, v);
+    };
+
+  return html`<div class="flex-1 flex flex-col justify-center h-96">
+    <div class="flex-1 flex flex-col justify-center items-center">
+      <div class="text-6xl">${verb}</div>
+      <div class="text-xl mt-2">${tense}</div>
+    </div>
+    <div class="flex flex-row gap-4 justify-center">
+      ${conjugations.map(
+        (conj, i) => html`<${InputValue}
+          conjugation=${conj}
+          pronoun=${PRONOUNS[i]}
+          onInput=${input(i)}
+          defaultValue=${values[i]}
+          value=${values[i]}
+        />`
+      )}
+    </div>
+  </div>`;
+}
+
+function pickVerb(n) {
+  const verb = ALL_VERBS[n % ALL_VERBS.length];
+  const tense = TENSES[(n / ALL_VERBS.length) | 0];
+  const conjugations = conjugate(verb)[tense];
+  return [verb, tense, conjugations];
+}
+
+function randomSeq(n) {
+  const total = ALL_VERBS.length * TENSES.length;
+  return new Array(n).fill().map(() => (Math.random() * total) | 0);
+}
+
+function PageButton({ side = 'right', disabled = false, onClick, ...rest }) {
   const text = side === 'right' ? '>' : '<';
   return html`<div
-    class="w-32 h-full flex justify-center items-center cursor-pointer hover:bg-sky-50"
+    class="w-32 h-full flex justify-center items-center ${disabled
+      ? 'text-gray-300'
+      : 'cursor-pointer hover:bg-sky-50'}"
+    onClick=${!disabled ? onClick : undefined}
+    ...${rest}
   >
     <button
       class="rounded-full border-2 w-10 h-10 bg-white"
       type="button"
       disabled=${disabled}
-      ${rest}
     >
       ${text}
     </button>
   </div>`;
 }
 
-function Exercise({ verb, tense, conjugations, fill }) {
-  return html`<div class="flex-1 flex flex-col justify-center h-96">
-    <div class="flex-1 flex flex-col justify-center items-center">
-      <div class="text-6xl">${verb}</div>
-      <div class="text-md">${tense}</div>
-    </div>
-    <div class="flex flex-row gap-4 justify-center">
-      ${conjugations.map(
-        (conj, i) =>
-          html`<${InputValue}
-            conjugation=${conj}
-            pronoun=${PRONOUNS[i]}
-            fill=${fill}
-          />`
-      )}
-    </div>
-  </div>`;
-}
-
-function pickRandomVerb() {
-  const n = (Math.random() * ALL_VERBS.length) | 0;
-  const verb = ALL_VERBS[n];
-  const tense = 'presente';
-  const conjugations = conjugate(verb)[tense];
-  return [verb, tense, conjugations];
-}
-
-const [verb, tense, conjugations] = pickRandomVerb();
-
 function App() {
-  const [cheat, setCheat] = useState(false);
+  const [seq, setSeq] = useState(randomSeq(10));
+  const [i, setI] = useState(0);
+
+  let [verb, tense, conjugations] = pickVerb(seq[i]);
+
+  const [values, setValues] = useState({
+    [verb]: new Array(conjugations.length).fill(''),
+  });
+
+  let allCorrect =
+    values[verb]?.every(
+      (currConj, i) => conjugations[i] == currConj.trim().toLowerCase()
+    ) ?? false;
+
+  let move = (n) => () => {
+    n = Math.max(0, Math.min(seq.length - 1, n));
+    if (i !== n) {
+      setI(n);
+    }
+  };
+
+  const exRef = useRef(null);
+
   useEffect(() => {
     const fn = (e) => {
       if (e.getModifierState('Control') && e.key === '0') {
-        setCheat(true);
+        exRef.current?.fill();
       }
     };
     window.addEventListener('keydown', fn);
@@ -148,14 +225,21 @@ function App() {
   });
 
   return html`<div class="h-screen flex flex-row items-center">
-    <${PageButton} side="left" />
+    <${PageButton} side="left" disabled=${i === 0} onClick=${move(i - 1)} />
     <${Exercise}
+      handleRef=${exRef}
+      key=${verb}
       verb=${verb}
       tense=${tense}
       conjugations=${conjugations}
-      fill=${cheat}
+      initialValues=${values[verb]}
+      onInput=${(v) => setValues({ ...values, [verb]: v })}
     />
-    <${PageButton} side="right" />
+    <${PageButton}
+      side="right"
+      disabled=${!allCorrect || i === seq.length - 1}
+      onClick=${move(i + 1)}
+    />
   </div>`;
 }
 
